f01731060d4ab2937fc76d513d74153d94aac6b9
Satoshiware <satoshiware@gmail.com>
2022-06-23 21:05:27 -0700

    files added to provide microcurrency options

---
+++ src/micros/micro.h
@@ -0,0 +1,7 @@
+/* Description:
+		Uncomment the microcurrency of choice. There can only be one.
+*/
+
+//#include <micros/micro_bitcoin.h>     // The one and only
+#include <micros/micro_testn.h>          // Microcurrency for testing
+//#include <micros/micro_azcoin.h>      // The Microcurrency of Arizona
