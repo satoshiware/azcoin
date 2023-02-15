d4264865dfa533936ca6210042251ee1170ae095
satoshiware <satoshiware@gmail.com>
2023-02-15 12:23:08 -0700

    Added the official AZ Money genesis block.

--- src/micros/micro_azmoney.h
+++ src/micros/micro_azmoney.h
@@ -13,12 +13,12 @@
 
 #define HALVINGINTERVAL		262800      // Bitcoin's Subsidy Halving Interval = 210000
 
-#define TIMESTAMP           "testing........."
-#define TIME                1673649471
-#define NONCE               1444837278
+#define TIMESTAMP           "BTC BLK: 0000000000000000000021bb823d8518bfa49c6f16bce1545c4977eb829238a9 TXID: b5f53d64..."
+#define TIME                1676412978
+#define NONCE               1429287480
 
-#define MERKLEHASH          "0x2def380691bdd8102e4eadbe83ad64f8fae3b316499dc1d59e2f76b9dbb9c739"
-#define GENESISHASH         "0x00000000b3a5be80d866af31dc487df43680426c73777a4d8d0525c5e74974ee"
+#define MERKLEHASH          "0xb9ed7f5a0f23a5063818064eb28979ca1a22fdbc38fbeb3726f759d83e82a69a"
+#define GENESISHASH         "0x00000000b00ff40d0f986a2314bbacbc003743b4b7062c6221b08256edc1ae94"
 
 // Magic bytes used to identify the communications within this microcurrency community.
 #define PCHMESSAGESTART0    0x81
@@ -31,18 +31,19 @@
 
 #endif // BITCOIN_MICROS_MICRO_AZCOIN_H
 
-
 /*
+python3 genesis.py -t 1676412978 -z "BTC BLK: 0000000000000000000021bb823d8518bfa49c6f16bce1545c4977eb829238a9 TXID: b5f53d64..." -v 1500000000
 algorithm: SHA256
-merkle hash: 2def380691bdd8102e4eadbe83ad64f8fae3b316499dc1d59e2f76b9dbb9c739
-pszTimestamp: testing.........
+merkle hash: b9ed7f5a0f23a5063818064eb28979ca1a22fdbc38fbeb3726f759d83e82a69a
+pszTimestamp: BTC BLK: 0000000000000000000021bb823d8518bfa49c6f16bce1545c4977eb829238a9 TXID: b5f53d64...
 pubkey: 04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f
-time: 1673649471 (Fri Jan 13 15:37:51 2023)
+time: 1676412978 (Tue Feb 14 15:16:18 2023)
 bits: 0x1d00ffff
 value: 1500000000
 Searching for genesis hash..
-422893 hash/s, estimate: 2.8 h, nonce: 1443999999 (max = 4294967295)
+1105063 hash/s, estimate: 1.1 h, nonce: 1428999999 (max = 4294967295)
 Genesis Hash Found!
-nonce: 1444837278
-Genesis Hash: 00000000b3a5be80d866af31dc487df43680426c73777a4d8d0525c5e74974ee
+nonce: 1429287480
+Genesis Hash: 00000000b00ff40d0f986a2314bbacbc003743b4b7062c6221b08256edc1ae94
 */
+// Proof of Existence for AZ Money whitepaper TXID: b5f53d6462f748de3bf17ef479b2855d1af67c90045f6ab6187062fb724f9c17
