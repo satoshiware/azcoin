bc26800241d123412d0d2646a59dc3a77fe1a229
Satoshiware <satoshiware@gmail.com>
2023-07-26 06:38:43 -0700

    Import public key output script for micros

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -543,8 +543,8 @@ public:
         m_assumed_chain_state_size = 0;
 
         const char* pszTimestamp = TIMESTAMP;
-        const CScript genesisOutputScript = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
-        genesis = CreateGenesisBlock(pszTimestamp, genesisOutputScript, TIME, NONCE, 0x1d00ffff, 1, BLOCKREWARD * COIN);
+        const CScript genesisOutputScript = CScript() << ParseHex(PUBKEYSCRIPT) << OP_CHECKSIG;
+        genesis = CreateGenesisBlock(pszTimestamp, genesisOutputScript, TIME, NONCE, DBITS, 1, BLOCKREWARD * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
         assert(consensus.hashGenesisBlock == uint256S(GENESISHASH));
         assert(genesis.hashMerkleRoot == uint256S(MERKLEHASH));
