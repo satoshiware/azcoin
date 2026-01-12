f774078433eac4160b9bbfad95e039eac07e626e
bcraypo <bcraypo@gmail.com>
2026-01-12 16:59:15 -0700

    Refactor InitParameterInteraction to clarify microcurrency parameters and enable outbound connectivity by default. Removed deprecated comments and streamlined logging for parameter interactions.

--- src/init.cpp
+++ src/init.cpp
@@ -643,48 +643,34 @@ static bool AppInitServers(NodeContext& node)
 // Parameter interaction based on rules
 void InitParameterInteraction(ArgsManager& args)
 {
-    // Parameters to help build a closed community during initial distribution of a new microcurrency
-    /******** Disabled to allow outbound connections ************/
-    /*if (args.IsArgSet("-micro") || args.GetArg("-chain", "") == "micro") {
-        if (args.SoftSetBoolArg("-connect", false))
-            LogPrintf("%s: parameter interaction: -micro set -> setting -connect=0\n", __func__);
-        if (args.SoftSetBoolArg("-listen", true))
+    // Parameters to help build a closed community during initial distribution of a new microcurrency.
+    //
+    // Note: older forks often forced a "closed" topology here (e.g. disabling discovery and/or requiring
+    // explicit peers). This project intentionally keeps outbound connectivity enabled by default.
+    if (args.IsArgSet("-micro") || args.GetArg("-chain", "") == "micro") {
+        if (args.SoftSetBoolArg("-listen", true)) {
             LogPrintf("%s: parameter interaction: -micro set -> setting -listen=1\n", __func__);
-        if (args.SoftSetBoolArg("-upnp", false))
-            LogPrintf("%s: parameter interaction: -micro set -> setting -upnp=0\n", __func__);
-        if (args.SoftSetBoolArg("-natpmp", false))
-            LogPrintf("%s: parameter interaction: -micro set -> setting -natpmp=0\n", __func__);
-        if (args.SoftSetBoolArg("-discover", false))
-            LogPrintf("%s: parameter interaction: -micro set -> setting -discover=0\n", __func__);
-        if (args.SoftSetBoolArg("-listenonion", false))
-            LogPrintf("%s: parameter interaction: -micro set -> setting -listenonion=0\n", __func__);
-        if (args.SoftSetBoolArg("-i2pacceptincoming", false)) {
-            LogPrintf("%s: parameter interaction: -micro set -> setting -i2pacceptincoming=0\n", __func__);
-        }*/
-    /******** Disabled to allow outbound connections ************/
-    // Parameters for micro chain defaults.
-// which effectively turns off automatic peer discovery/connection.
-        if (args.IsArgSet("-micro") || args.GetArg("-chain", "") == "micro") {
-            if (args.SoftSetBoolArg("-listen", true))
-                LogPrintf("%s: parameter interaction: -micro set -> setting -listen=1\n", __func__);
-
-            // Enable normal autoconnect/discovery by default.
-            if (args.SoftSetBoolArg("-discover", true))
-                LogPrintf("%s: parameter interaction: -micro set -> setting -discover=1\n", __func__);
-
-            // Keep these conservative defaults unless you specifically want them on.
-            if (args.SoftSetBoolArg("-upnp", false))
-                LogPrintf("%s: parameter interaction: -micro set -> setting -upnp=0\n", __func__);
-            if (args.SoftSetBoolArg("-natpmp", false))
-                LogPrintf("%s: parameter interaction: -micro set -> setting -natpmp=0\n", __func__);
-
-            if (args.SoftSetBoolArg("-listenonion", false))
-                LogPrintf("%s: parameter interaction: -micro set -> setting -listenonion=0\n", __func__);
-            if (args.SoftSetBoolArg("-i2pacceptincoming", false)) {
-                LogPrintf("%s: parameter interaction: -micro set -> setting -i2pacceptincoming=0\n", __func__);
-            }
         }
 
+        // Enable normal autoconnect/discovery by default.
+        if (args.SoftSetBoolArg("-discover", true)) {
+            LogPrintf("%s: parameter interaction: -micro set -> setting -discover=1\n", __func__);
+        }
+
+        // Keep these conservative defaults unless you specifically want them on.
+        if (args.SoftSetBoolArg("-upnp", false)) {
+            LogPrintf("%s: parameter interaction: -micro set -> setting -upnp=0\n", __func__);
+        }
+        if (args.SoftSetBoolArg("-natpmp", false)) {
+            LogPrintf("%s: parameter interaction: -micro set -> setting -natpmp=0\n", __func__);
+        }
+
+        if (args.SoftSetBoolArg("-listenonion", false)) {
+            LogPrintf("%s: parameter interaction: -micro set -> setting -listenonion=0\n", __func__);
+        }
+        if (args.SoftSetBoolArg("-i2pacceptincoming", false)) {
+            LogPrintf("%s: parameter interaction: -micro set -> setting -i2pacceptincoming=0\n", __func__);
+        }
     }
 
     // when specifying an explicit binding address, you want to listen on it
