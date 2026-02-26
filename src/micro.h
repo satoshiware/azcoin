5941605c8a5ff4724cb67c00ba39ec47d9ac1cbe
satoshiware <satoshiware@gmail.com>
2026-02-26 06:16:59 -0700

    migrating micro (azmoney) to azcoin

--- src/micro.h
+++ src/micro.h
@@ -2,30 +2,25 @@
 		First microcurrency! Launched and distributed within U.S.A. !
 */
 
-#ifndef BITCOIN_MICROS_MICRO_DESERETMONEY_H
-#define BITCOIN_MICROS_MICRO_DESERETMONEY_H
+#ifndef BITCOIN_MICROS_MICRO_AZCOIN_H
+#define BITCOIN_MICROS_MICRO_AZCOIN_H
 
-#define MICROCURRENCY       "deseretmoney"
-#define BECH32HRP           "ut"
+#define MICROCURRENCY       "azcoin"
+#define BECH32HRP           "az"
 
-<<<<<<< HEAD
-#define BLOCKREWARD         7          // Bitcoin's Block Reward = 50. Note: to run tests, it must be 50!
-#define MAXSUPPLY           3564225    // Bitcoin's Max Supply = 21000000
-=======
 #define BLOCKREWARD         15          // Bitcoin's Block Reward = 50. Note: to run tests, it must be 50!
 #define MAXSUPPLY           7884000    	// Bitcoin's Max Supply = 21000000
->>>>>>> feature/docker-harden-and-azcoin-wrappers
 
 #define HALVINGINTERVAL		262800      // Bitcoin's Subsidy Halving Interval = 210000
 
 #define TIMESTAMP           "BTC BLK: 0000000000000000000021bb823d8518bfa49c6f16bce1545c4977eb829238a9 TXID: b5f53d64..."
 #define PUBKEYSCRIPT		"04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f"
-#define TIME                1690242443
-#define DBITS               0x1c00ff00
-#define NONCE               1960094637
+#define TIME                1676412978
+#define DBITS               0x1d00ffff
+#define NONCE               1429287480
 
-#define MERKLEHASH          "0xf19430c99d84d77f07c9183c802b147ed9e183840e0df93d3d51c9948b7ed076"
-#define GENESISHASH         "0x00000000006854af34b85cccb4bb867ae0095aa714fc34a9a7a9ca243897cbc2"
+#define MERKLEHASH          "0xb9ed7f5a0f23a5063818064eb28979ca1a22fdbc38fbeb3726f759d83e82a69a"
+#define GENESISHASH         "0x00000000b00ff40d0f986a2314bbacbc003743b4b7062c6221b08256edc1ae94"
 
 // Magic bytes used to identify the communications within this microcurrency community.
 #define PCHMESSAGESTART0    0x81
